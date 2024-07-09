5581b2fa8bf7897e2a83d687814e9479f2a5e7e8
Herwiggg <michelot.alexy78@gmail.com>
2024-07-09 03:57:40 +0200

    ex01

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -6,13 +6,38 @@
 /*   By: almichel <almichel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 18:28:05 by almichel          #+#    #+#             */
-/*   Updated: 2024/07/09 03:48:52 by almichel         ###   ########.fr       */
+/*   Updated: 2024/07/09 03:56:40 by almichel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string.h>
 #include "Contact.class.hpp"
+
+
+void addContact(Contact &contact) {
+    std::string input;
+    
+    std::cout << "> First name : ";
+    std::getline(std::cin, input);
+    contact.setFirstName(input);
+    
+    std::cout << "> Last name : ";
+    std::getline(std::cin, input);
+    contact.setLastName(input);
+    
+    std::cout << "> Nickname : ";
+    std::getline(std::cin, input);
+    contact.setNickName(input);
+    
+    std::cout << "> Phone number : ";
+    std::getline(std::cin, input);
+    contact.setPhoneNumber(input);
+    
+    std::cout << "> Darkest secret : ";
+    std::getline(std::cin, input);
+    contact.setDarkestSecret(input);
+}
 
 int main(int ac, char **av)
 {
@@ -26,22 +51,6 @@ int main(int ac, char **av)
 		if (action == "EXIT")
 			break;
 		else if(action == "ADD")
-		{
-			std::cout << "> First name : ";
-			getline(std::cin, action);
-			contact.setFirstName(action);
-			std::cout << "> Last name : ";
-			getline(std::cin, action);
-			contact.setLastName(action);
-			std::cout << "> Nickname : ";
-			getline(std::cin, action);
-			contact.setNickName(action);
-			std::cout << "> Phone number : ";
-			getline(std::cin, action);
-			contact.setPhoneNumber(action);
-			std::cout << "> Darkest secret :  ";
-			getline(std::cin, action);
-			contact.setDarkestSecret(action);
-		}
+			addContact(contact);
 	}
 }
