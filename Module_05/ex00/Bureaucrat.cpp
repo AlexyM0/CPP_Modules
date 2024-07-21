659cc10d5ddc52b2c4a5a7835b813f65e0cd3ca6
Herwiggg <michelot.alexy78@gmail.com>
2024-07-21 04:28:48 +0200

    ex00

---
+++ Module_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,63 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
+{
+    checkGrade();
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &src) : _name(src._name), _grade(src._grade)
+{
+
+}
+
+Bureaucrat::~Bureaucrat()
+{
+
+}
+
+int Bureaucrat::getGrade(void) const
+{
+    return (this->_grade);
+}
+
+std::string Bureaucrat::getName() const
+{
+    return (this->_name);
+}
+
+void Bureaucrat::incrementGrade(void)
+{
+    this->_grade++;
+    checkGrade();
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+    this->_grade--;
+    checkGrade();
+}
+
+void   Bureaucrat::checkGrade(void)
+{
+    if (this->_grade > 150)
+        throw Bureaucrat::GradeTooHighException();
+    if (this->_grade < 1)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is to high");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is to low");
+}
+
+std::ostream &	operator<<(std::ostream & o, Bureaucrat const & rhs)
+{
+	o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+	return o;
+}
+
