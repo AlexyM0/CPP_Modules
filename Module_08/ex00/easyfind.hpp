c159734e7cd5f726f9e4a91660f1ad1e8144a87c
Alexy Michelot <almichel@paul-f4Ar6s6.clusters.42paris.fr>
2025-03-24 17:05:00 +0100

    Module 08

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -3,14 +3,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <list>
+#include <algorithm>
 template<typename T>
-T easyfind(T const std::list& lst, int val2)
+typename T::iterator easyfind(T &container, int val2)
 {
-    for (size_t i = 0; i < lst.size(), i++)
-    {
-        if (lst[i] == val2)
-            return (<T>std::cout << "The first occurence of" << val2 << "has been found at the element numero" << i << std::end;);
-    }
-
+    typename T::iterator it  = std::find(container.begin(), container.end(), val2);
+	return it;
 }
--- Module_08/ex00/main.cpp
+++ Module_08/ex00/main.cpp
@@ -2,6 +2,29 @@
 
 int main()
 {
-    std::list<int> lst = {1, 2, 3, 4, 5};
-    easyfind(<T>lst, 4)
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+
+    std::list<int> lst;
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+    lst.push_back(40);
+    lst.push_back(50);
+
+    int vec_to_find = 3;
+    std::vector<int>::iterator it_vec = easyfind(vec, vec_to_find);
+    if (it_vec != vec.end())
+        std::cout << "Valeur trouvée dans le container : " << *it_vec << std::endl;
+
+    int lst_to_find = 25;
+    std::list<int>::iterator it_lst = easyfind(lst, lst_to_find);
+    if (it_lst != lst.end())
+        std::cout << "Valeur trouvée dans le container : " << *it_lst << std::endl;
+    else
+        std::cout << lst_to_find << " n'a pas été trouvée dans le container" <<std::endl;
 }
