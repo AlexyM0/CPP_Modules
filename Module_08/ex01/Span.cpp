c5b575449db0fb4833251c05a591222e84943a82
Alexy Michelot <almichel@paul-f4Ar6s6.clusters.42paris.fr>
2025-03-24 18:28:03 +0100

    Module 08

---
+++ Module_08/ex01/Span.cpp
@@ -0,0 +1,17 @@
+#include "Span.hpp"
+
+Span::Span()
+{
+    this->_size = 0;
+}
+
+Span::Span(unsigned int size)
+{
+    this->_size = size;
+
+}
+
+Span::~Span()
+{
+    
+}
---
+++ Module_08/ex01/Span.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "iostream"
+
+class Span
+{
+    public:
+        Span();
+        Span(unsigned int);
+        ~Span();
+        addNumber();
+        shortestSpan();
+        longestSpan();
+    
+    private:
+        unsigned int _size;
+
+
+};
